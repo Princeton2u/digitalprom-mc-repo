3ddab808173d1e3398653787bba8928340f470d7
hattab2k@gmail.com <hattab2k@gmail.com>
2013-03-06 18:00:07 +0400

    Update ivi.ru and zoomby.ru plugins

--- master/addons/plugin.video.lapti.tv/default.py
+++ master/addons/plugin.video.lapti.tv/default.py
@@ -5,55 +5,72 @@
 
 
 
-import urllib,urllib2,re,sys,os,time
-import xbmcplugin,xbmcgui,xbmcaddon
+import urllib
+import urllib2
+import sys
+import os
+import re
+import xbmcplugin
+import xbmcgui
+import xbmc
+import xbmcaddon
+import time
+import random
+import threading
+from StringIO import StringIO
+import gzip
 
 pluginhandle = int(sys.argv[1])
-__addon__       = xbmcaddon.Addon(id='plugin.video.inetcom.tv') 
-#fanart    = xbmc.translatePath( __addon__.getAddonInfo('path') + 'fanart.jpg')
-#xbmcplugin.setPluginFanart(pluginhandle, fanart)
+Addon         = xbmcaddon.Addon(id='plugin.video.lapti.tv')
+addon_id      = Addon.getAddonInfo('id')
+addon_icon    = Addon.getAddonInfo('icon')
 
-INC_url = 'http://lapti.tv'
-INC_ch = '/'
-
+INC_url = 'http://lapti.tv/'
 
 dbg = 1
 def dbg_log(line):
   if dbg: xbmc.log(line)
 
-def get_url(url, data = None, cookie = None, save_cookie = False, referrer = None):
-    req = urllib2.Request(url)
-    req.add_header('User-Agent', 'Opera/9.80 (X11; Linux i686; U; ru) Presto/2.7.62 Version/11.00')
-    req.add_header('Accept', 'text/html, application/xml, application/xhtml+xml, */*')
-    req.add_header('Accept-Language', 'ru,en;q=0.9')
+def ShowMessage(heading, message, times = 3000, pics = addon_icon):
+    try: xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s, "%s")' % (heading.encode('utf-8'), message.encode('utf-8'), times, pics.encode('utf-8')))
+    except Exception, e:
+        print( '[%s]: ShowMessage: Transcoding UTF-8 failed [%s]' % (addon_id, e), 2 )
+        try: xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s, "%s")' % (heading, message, times, pics))
+        except Exception, e:
+            print( '[%s]: ShowMessage: exec failed [%s]' % (addon_id, e), 3 )
 
-    if cookie: req.add_header('Cookie', cookie)
-    if referrer: req.add_header('Referer', referrer)
-    if data: 
-        response = urllib2.urlopen(req, data)
-    else:
-        response = urllib2.urlopen(req)
-    link=response.read()
+def get_url(target, post=None):
+    try:
+        request = urllib2.Request(url = 'http://www.lapti.tv', data = post,  headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.2; rv:19.0) Gecko/20121218 Firefox/19.0'})
+        request.add_header('User-Agent', 'Opera/9.80 (X11; Linux i686; U; ru) Presto/2.7.62 Version/11.00')
+        request.add_header('Accept', 'text/html, application/xml, application/xhtml+xml, */*')
+        request.add_header('Accept-Language', 'ru,en;q=0.9')
+        response = urllib2.urlopen(request)
+        if response.info().get('Content-Encoding') == 'gzip':
+            buf = StringIO( response.read())
+            f = gzip.GzipFile(fileobj=buf)
+            http = f.read()
+        else:
+            http = response.read()
+        response.close()
+        return http
+    except Exception, e:
+        print( '[%s]: GET EXCEPT [%s]' % (addon_id, e), 4 )
+        ShowMessage('HTTP ERROR', e, 5000)
+        #print('{LOG}[GET]:Error')
 
-    if save_cookie:
-        setcookie = response.info().get('Set-Cookie', None)
-        if setcookie:
-            setcookie = re.search('([^=]+=[^=;]+)', setcookie).group(1)
-            link = link + '<cookie>' + setcookie + '</cookie>'
-
-    response.close()
-    return link
-
-        
 def INC_prls(url):
     dbg_log('INC_prls')
-    
+   
     http = get_url(url)
     oneline = re.sub('[\r\n\t]', ' ', http)
     ch_cont = re.compile('<div class="ch-wrappers ch-center"> +?<ul> +?(.+?) +?</ul> +?</div>').findall(oneline)
-    pr_ls = re.compile('<a class="(.+?)" +?href="(.+?)"></a>').findall(ch_cont[0])
+    if len(ch_cont):
+        pr_ls = re.compile('<a class="(.+?)" +?href="(.+?)"></a>').findall(ch_cont[0])
+    else:
+        pr_ls = None
 
-    if len(pr_ls):
+    if pr_ls:
         for descr, href in pr_ls:
             name = descr
             dbg_log(name)
@@ -62,20 +79,16 @@ def INC_prls(url):
             uri += '&url='+urllib.quote_plus(INC_url + href)
             item.setInfo( type='video', infoLabels={'title': name, 'plot': descr})
             item.setProperty('IsPlayable', 'true')
-            dbg_log(uri)
             xbmcplugin.addDirectoryItem(pluginhandle, uri, item)
-
     xbmcplugin.endOfDirectory(pluginhandle)    
     
 def INC_play(url, name, thumbnail, plot, mycookie):
     dbg_log('-INC_play')
     response = get_url(url)
     lnks_ls = re.compile('http://([^|^&]+?)/playlist\.m3u8').findall(response)
-
     if len(lnks_ls):
         item = xbmcgui.ListItem(path = 'http://' + lnks_ls[0] + '/playlist.m3u8')
         xbmcplugin.setResolvedUrl(pluginhandle, True, item)
-             
    
 def get_params():
     param=[]
@@ -132,11 +145,8 @@ except: pass
 
 
 
-
-
-
 if mode == 'PLAY': INC_play(url, name, thumbnail, plot, cook)
-elif mode == None: INC_prls(INC_url + INC_ch)
+elif mode == None: INC_prls(INC_url)
 
 dbg_log('CLOSE:')
 
